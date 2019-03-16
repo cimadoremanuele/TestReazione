74640ee874d04934f728d81453368aa4a3734091
Cimador Emanuele <emanuele.cimador@barsanti.edu.it>
2019-03-16 09:27:39 +0100

    inserimento codice buzzer

--- TestReazione.ino
+++ TestReazione.ino
@@ -1,22 +1,31 @@
 #include <LiquidCrystal_I2C.h>
-int ledStart = 13;
+//int ledStart = 13;
 int button1;
 int led1;
 int button2;
 int led2;
 int ledRGB;
-int btnStart = 7;
-int buttonstatus = 0;
+int beep;
+int tempo1;
+int tempo2;
+int btnStart;
+int buttonstatus;
 int tempoLed;
+int tempoBeep;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   // put your setup code here, to run once:
+  //assegno il numero dei pin alle variabili dichiarate sopra
   lcd.init();
   lcd.backlight();
   pinMode(btnStart, INPUT);
-  pinMode(ledStart, OUTPUT);
+  pinMode(button1, INPUT);
+  pinMode(led1,OUTPUT);
+  pinMode(button2, INPUT);
+  pinMode(beep, OUTPUT);
+  pinMode(ledRGB, OUTPUT);
 }
 
 void loop() {
@@ -24,10 +33,39 @@ void loop() {
   buttonstatus = digitalRead(btnStart);
   if (buttonstatus == HIGH)
   {
-    digitalWrite(ledStart, LOW);
+    digitalWrite(led1, LOW);
     tempoLed = random(2000,5000);
     delay(tempoLed);
-    digitalWrite(ledStart, HIGH);
+    digitalWrite(led1, HIGH);
+  }
+  buttonstatus = digitalRead(button1);
+  while(led1 == HIGH)
+  {
+    if (buttonstatus == HIGH)
+    {
+      digitalWrite(led1, LOW);
+      tempoBeep = random(2000,5000);
+      delay(tempoBeep);
+      digitalWrite(beep, HIGH);
+    }
+    else
+    {
+      tempo1++;
+      delay(1);
+    }
+  }
+  buttonstatus = digitalRead(button2);
+  while (beep == HIGH)
+  {
+    if (buttonstatus == HIGH)
+    {
+      digitalWrite(beep, LOW);
+    }
+    else
+    {
+      tempo2++;
+      delay(1);
+    }
   }
   lcd.setCursor(0, 0);
   lcd.print("Ciao");
