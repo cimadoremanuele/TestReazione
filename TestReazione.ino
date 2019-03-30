471689c5eb17b245290b05bf1b1fac19a1f10578
Cimador Emanuele <emanuele.cimador@barsanti.edu.it>
2019-03-30 08:13:35 +0100

    inserimento commenti

--- TestReazione.ino
+++ TestReazione.ino
@@ -1,28 +1,26 @@
 #include <LiquidCrystal_I2C.h>
-int btnStart;
-int led1;
-int button1;
-int beep;
-int button2;
-int ledR;
-int ledV;
-int tempo1;
-int tempo2;
-int tempoLed;
-int tempoBeep;
+int btnStart; //bottone blu
+int led1;     //led blu
+int button1;  //bottone giallo
+int beep;     //buzzer
+int button2;  //bottone verde
+int ledR;     //led RGB
+int ledV;     //led RGB
+int tempo1;   //tempo led
+int tempo2;   //tempo buzzer
 bool errore;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   // put your setup code here, to run once:
   //assegno il numero dei pin alle variabili dichiarate sopra
+  ledR = 5;
+  ledV = 6;
   btnStart = 7;
   led1 = 8;
   button1 = 9;
   beep = 10;
   button2 = 11;
-  ledR = 5;
-  ledV = 6;
   errore = false;
   lcd.init();
   lcd.backlight();
@@ -37,12 +35,15 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //frase iniziale
   lcd.setCursor(0,0);
   lcd.print("Premi il bottone");
   lcd.setCursor(0,1);
   lcd.print("blu per iniziare");
+  //inizio
   if (digitalRead(btnStart) == HIGH)
   {
+    //pulisco il display, azzero i tempi e spengo il led RGB
     lcd.clear();
     errore = false;
     tempo1 = 0;
@@ -50,91 +51,97 @@ void loop() {
     digitalWrite(ledV, LOW);
     digitalWrite(ledR, LOW);
     ButtonClicked(tempoLed,led1, button1);
-    if (errore == true)
-    {
-      lcd.print("Stai barando,");
-      lcd.setCursor(0,1);
-      lcd.print("ricomincia!");
-      digitalWrite(ledR, HIGH);
-    }
-    else
-    {
-      while (digitalRead(button1) == LOW)
-  {
-    tempo1++;
-    delay(1);
-  }
-  digitalWrite(led1, LOW);
-  Scrittura(tempo1, 0, 0, "led");
-  if (digitalRead(button1) == HIGH)
-  {
-    ButtonClicked(tempoBeep,beep, button2);
+    //intervengo se la persona sta barando
     if (errore == true)
     {
       Errore();
     }
     else
     {
-      while (digitalRead(button2) == LOW)
+      //calcolo il tempo di reazione
+      while (digitalRead(button1) == LOW)
       {
-        tempo2++;
+        tempo1++;
         delay(1);
-        }
-        Scrittura(tempo2,0,1, "suono");
-        if (digitalRead(button2) == HIGH)
+      }
+      //scrivo sul display il risultato
+      digitalWrite(led1, LOW);
+      Scrittura(tempo1, 0, 0, "led");
+      if (digitalRead(button1) == HIGH)
+      {
+        ButtonClicked(tempoBeep,beep, button2);
+        //intervengo se la persona sta barando
+        if (errore == true)
         {
-          digitalWrite(beep, LOW);
+          Errore();
+        }
+        else
+        {
+          //calcolo il tempo di reazione
+          while (digitalRead(button2) == LOW)
+          { 
+            tempo2++;
+            delay(1);
+          }
+          //scrivo sul display il risultato
+          Scrittura(tempo2,0,1, "suono");
+          if (digitalRead(button2) == HIGH)
+          {
+            digitalWrite(beep, LOW);
+          }
+        }
+        //scrivo sul display il risultato
+        if (errore == true)
+        {
+          Errore();
+        }
+        else if(tempo1 > 500 || tempo2 > 500)
+        {
+          digitalWrite(ledR, HIGH);
+          delay(3000);
+          lcd.clear();
+          lcd.setCursor(0,0);
+          lcd.print("Test non");
+          lcd.setCursor(0,1);
+          lcd.print("superato!");
+        }
+        else
+        {
+          digitalWrite(ledV, HIGH);
+          delay(3000);
+          lcd.clear();
+          lcd.setCursor(0,0);
+          lcd.print("Test superato!");
         }
       }
-      if (errore == true)
-      {
-        Errore();
-      }
-      else if(tempo1 > 500 || tempo2 > 500)
-      {
-        digitalWrite(ledR, HIGH);
-        delay(3000);
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("Test non");
-        lcd.setCursor(0,1);
-        lcd.print("superato!");
-      }
-      else
-      {
-        digitalWrite(ledV, HIGH);
-        delay(3000);
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("Test superato!");
-      }
-    }
     }
     delay(2000);
     digitalWrite(ledV, LOW);
     digitalWrite(ledR, LOW);
-    }
+  }
 }
 void ButtonClicked(int tempo, int output, int button)
-{
-    tempo = random(2000,5000);
-    int tempo1 = 0;
-    while (tempo1 <= tempo)
+{ 
+  //calcolo il tempo tra l'accensione del led e il bottone premuto
+  tempo = random(2000,5000);
+  int tempo1 = 0;
+  while (tempo1 <= tempo)
+  {
+    if (digitalRead(output) == LOW && digitalRead(button) == HIGH)
     {
-      if (digitalRead(output) == LOW && digitalRead(button) == HIGH)
-      {
-        errore = true;
-      }
-      tempo1++;
-      delay(1);
+      errore = true;
     }
-    if (errore != true)
-    {
-      digitalWrite(output, HIGH);
-    }
+    tempo1++;
+    delay(1);
+  }
+  if (errore != true)
+  {
+    digitalWrite(output, HIGH);
+  }
 }
 void Scrittura(int tempo, int a, int b, String tipo)
 {
+  //scrivo il risultato
   lcd.setCursor(a, b);
   lcd.print(tipo + ":" + String(tempo) + "ms");
 }
